41e3c0ec52cf466e83a9e86918cb49130ce2e96a
zhujiangleo-dotcom <zhujiangleo@gmail.com>
2025-11-26 15:44:08 +0800

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,1 +1,9 @@
+import { defineCollection } from 'astro:content';
+import { docsSchema } from '@astrojs/starlight/schema';
+
+export const collections = {
+  docs: defineCollection({
+    schema: docsSchema(),
+  }),
+};
 
